870c93fec553d018f7c3b1ee1d3fa5efa8873fb4
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-05-19 11:42:47 +0200

    feat: updated readme file

--- obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a715390914635c96d975038eb056fee88354d22f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1eb789a9d3189ca9e6b7a2c38f3e1131c158ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
